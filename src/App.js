4444bfba99a98965fb26b15271fc4a637d5b1a12
Grady Robbins <gradyrobbins13@gmail.com>
2019-02-06 10:31:36 -0600

    login form renders when navigate to /login AND verifies&stores credentials

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
+
+
 import React, { Component } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 import './App.css';
-import map from './img/USMap.png'
+// import map from './img/USMap.png'
 import coverImage from './img/coverImage.jpg'
+import ApplicationViews from './ApplicationViews'
 
 
 class App extends Component {
   render() {
     return (
+      <React.Fragment>
+        {/* <NavBar /> */}
+        <ApplicationViews />
       <div className="App">
 
         <section>
-          <img src={map} alt="coverImage" className="coverImage"></img>
+          {/* <img src={map} alt="coverImage" className="coverImage"></img> */}
           <img src={coverImage} alt="coverImage" className="coverImage"></img>
         </section>
+
       </div>
+
+      </React.Fragment>
+
+
     );
+
+
   }
 }
 

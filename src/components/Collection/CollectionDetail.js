1d55b12c6bfd7fbf4b1eb6eb3b110435069f9645
Grady Robbins <gradyrobbins13@gmail.com>
2019-02-12 09:29:38 -0600

    changes and stuff

--- src/components/Collection/CollectionDetail.js
+++ src/components/Collection/CollectionDetail.js
@@ -10,7 +10,8 @@ import DataManager from "../../modules/DataManager";
 export default class CollectionDetail extends Component {
     state ={
         matchlist: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55],
-        userSpecific: []
+        userSpecific: [],
+        needsList: []
   }
 
   componentDidMount() {
@@ -21,8 +22,7 @@ export default class CollectionDetail extends Component {
     .then(() => this.setState(newState))
 };
 
-// .then()
-//setState here with another .then statement.  below, use this.state.qu to map into <Has Card />
+//  below, use this.state.userSpecific to map onto <Has Card />
 
 
     render() {
@@ -53,9 +53,9 @@ export default class CollectionDetail extends Component {
                         <button>Add a new Quarter to {collection.collectorsName}'s Collection</button>
                     </div>
                 </section>
+                        <button>See all Collections</button>
                 <br/>
                 <br/>
-                <section>Has Card goes here</section>
 
 
                 <HasCard

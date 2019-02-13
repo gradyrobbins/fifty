395c8debcc58d503dface37b4dddc9849c82f1cf
Grady Robbins <gradyrobbins13@gmail.com>
2019-02-13 11:01:19 -0600

    CSS card-borders on < Learn />

--- src/components/Learn/Learn.js
+++ src/components/Learn/Learn.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import NavBar from "./../Nav/NavBar";
-
+import './Learn.css'
 class List extends Component {
 
     render() {
@@ -11,20 +11,24 @@ class List extends Component {
             <br/>
             <br/>
                 <NavBar />
-
+            <br/>
+            <br/>
+            <h3> &lt; Clickable USA Map component / &gt; goes here </h3>
+            <h4> whichever state is clicked, renders the corresponding card and information </h4>
 
             <div>
                 {this.props.usas.map(item => {
                     // console.log(item)
                     return <div className="Card" key={item.id}>
-                                {item.name} <br/><br/>
+                               <h4><strong> {item.name} </strong></h4> <br/><br/>
                                 Date of Statehood: {item.Statehood} <br/>
                                 Capital: {item.Capital} <br/>
                                 Nicknames: {item.Nicknames} <br/>
                                 Motto: {item.Motto} <br/>
                                 Flower: {item.Flower} <br/>
                                 Bird: {item.Bird} <br/>
-                                </div>
+                                &lt; IMPORT AN IMAGE OF THE STATE'S BORDERS HERE &gt;
+                            </div>
                         })
                 }
 
